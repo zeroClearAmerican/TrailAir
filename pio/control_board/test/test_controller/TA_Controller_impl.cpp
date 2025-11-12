64ffb2ea5872079f7014505e3a0120f3d4a094be
zeroClearAmerican <35619771+zeroClearAmerican@users.noreply.github.com>
2025-11-11 23:07:59 -0600

    g

---
+++ pio/control_board/test/test_controller/TA_Controller_impl.cpp
@@ -0,0 +1,2 @@
+// Include TA_Controller implementation for native tests
+#include "../../pioLib/TA_Controller/src/TA_Controller.cpp"
---
+++ pio/remote/test/test_ui/TA_UI_impl.cpp
@@ -0,0 +1,2 @@
+// Include TA_UI implementation for native tests
+#include "../../pioLib/TA_UI/src/TA_UI.cpp"
